42809577553fe6289c4cc146f6fe0f90a28407c4
Milajj <149986465+Milajj@users.noreply.github.com>
2024-01-12 17:08:46 -0500

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
+<!-- 
+    - IRM4900 Capstone Project - Fall 2023/ Winter 2024
+    - Using Generative Artificial Intelligence for Age-Appropriate Text Summarization
+
+    - Team Members  
+    - Technical Lead: Jamila Johnson | 101206365
+    - Documentation: Phoebe Chong | 101075924 
+    - Lead Developer/AI: Ilona Krasnova | 101211529
+    - UI/UX Designer: Morgan Gardiner | 101024884
+
+    - Rescribe's contact page. Displaying that the form has been sent 
+-->
+
 <html lang="en">
 
+    <!--Head element used to contain metadata, estabilish the website's title and import google fonts-->
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +27,7 @@
 
     <body>
 
+        <!--Website toolbar: allows user the ability to redirect to other pages of the website-->
         <div class="gridContainer">
             <div class="header">
                 <img src="images/icons8-feather-48.png" class="featherIcon" alt="Rescribe icon - a feather">        
@@ -25,10 +40,8 @@
             </div><!--header end-->
 
         
-           <img src="images/hootMessage.png" alt="Thank you for contact us" class="subMessage">
-
-
-            <img src="images/hootSub.png" alt="Owl mascot" class="subOwl">
+           <!--<img src="images/hootMessage.png" alt="Thank you for contact us" class="subMessage">
+            <img src="images/hootSub.png" alt="Owl mascot" class="subOwl">-->
 
             
 
