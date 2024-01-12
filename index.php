7acbc2443618bb0952f293b4679e5ea02ca8079e
Milajj <149986465+Milajj@users.noreply.github.com>
2024-01-12 17:09:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
+<!-- 
+    - IRM4900 Capstone Project - Fall 2023/ Winter 2024
+    - Using Generative Artificial Intelligence for Age-Appropriate Text Summarization
+
+    - Team Members  
+    - Technical Lead: Jamila Johnson | 101206365
+    - Documentation: Phoebe Chong | 101075924 
+    - Lead Developer/AI: Ilona Krasnova | 101211529
+    - UI/UX Designer: Morgan Gardiner | 101024884
+
+    - Rescribe's homepage. Function: A web-based educational tool that simplifies keywords and summarises complex text documents. 
+-->
+
 <html lang="en">
 
+    <!--Head element used to contain metadata, estabilish the website's title and import google fonts-->
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +27,7 @@
 
     <body>
 
+         <!--Website toolbar: allows user the ability to redirect to other pages of the website-->
         <div class="gridContainer">
             <div class="header">
                 <img src="images/icons8-feather-48.png" class="featherIcon" alt="Rescribe icon - a feather">        
@@ -24,6 +39,9 @@
                     </div><!--headerRight end-->
             </div><!--header end-->
 
+            <!--HTML form that uses a HTTP post request to send user request/ API request.
+                The form data is sent for processing to a PHP file "output.php".
+            -->
             <form method="post" action="output.php" class="formElement">
                     <textarea class="textAInput" id="textAI" name="textArea" placeholder="Type text here..."></textarea>
                     <input type="range" min="1" max="3" value="1" class="diffSlider" id="dSlider" name="slider" onchange="changingSlider()">
@@ -43,6 +61,10 @@
 
         <!--clear button function--> 
         <script>
+            /*clear button function: clears the contents of the teatarea.
+            Dsiplay and update slider value - reassign slider's predefined numerical values as Easy, Medium and Hard
+            */
+
             function clearText(){
                 document.getElementById('textAI').value = '';
             }
