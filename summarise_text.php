e060df58d1fa81280442193dfeebc0b899a23983
Ilona Krasnova <ilona.a.krasnova@gmail.com>
2024-02-17 16:47:55 -0500

    summarise_text as loading page

--- summarise_text.php
+++ summarise_text.php
@@ -1,10 +1,30 @@
+<html>
+<head>
+    <title>Processing</title>
+  </head>
+    <body>
+        <pre>
+
+                 ^...^      Processing!
+                / o,o \
+                |):::(|
+                ====w=w===
+        </pre>
+    </body>
+</html>
 <?php
 include("begin.php");
 $num = $_REQUEST['num'] ?? 600;
+$text = $_REQUEST['text'];
+$difficulty_level = $_REQUEST['level'];
+
+if (empty($text) || empty($difficulty_level)){
+    echo "Text and level parameters are required";
+    include("end.php");
+    exit();
+}
 
 $task1 = "Write a very short (less then 160 words) summary of the following article. You audience is 6th grade school students. Use clear and simple language, even when explaining complex topics. Bias toward short sentences. Avoid jargon.";
-
-$text = "Language documentation involves recording the speech of native speakers. Transcribing these recordings, which are rich cultural and linguistic resources, is an integral part of the language documentation process. However, transcription is slow: it often takes a linguist between 30 minutes to 2 hours to transcribe and translate a minute of speech, depending on the transcriber’s familiarity with the language and the difficulty of the content. This is a bottleneck in the documentary linguistics workflow: linguists accumulate considerable amounts of speech, but do not transcribe and translate it all, and there is a risk that untranscribed recordings could end up as “data graveyards” (Himmelmann, 2006, 4,12-13). There is clearly a need for “devising better ways for linguists to do their work” (Thieberger, 2016, 92). There has been work on low-resource speech recognition (Besacier et al., 2014), with approaches using cross-lingual information for better acoustic modelling (Burget et al., 2010; Vu et al., 2014; Xu et al., 2016; Müller et al., 2017) and language modelling (Xu and Fung, 2013). However, speech recognition technology has largely been ineffective for endangered languages since architectures based on hidden Markov models (HMMs), which generate orthographic transcriptions, require a large pronunciation lexicon and a language model trained on text. These speech recognition systems are usually trained on a variety of speakers and hundreds of hours of data (Hinton et al., 2012, 92), with the goal of generalisation to new speakers. Since large amounts of text are used for language model training, such systems often do not incorporate pitch information for speech recognition of tonal languages (Metze et al., 2013), as they can instead rely on contextual information for tonal disambiguation via the language model (Le and Besacier, 2009; Feng et al., 2012) even though there is no computational burden in additionally using pitch features. In contrast, language documentation contexts often have just a few speakers for model training and little text for language model training. However, there may be benefit even in a system that overfits to these speakers. If a phonemic recognition tool can provide a canvas transcription for manual correction and linguistic analysis, it may be possible to improve the workflow and leverage of linguists. The transcriptions collected in this semi-automated workflow can then be used for refinement of the acoustic model, leading to a snowball effect of better and faster transcription. In this paper we investigate the application of neural speech recognition models to the task of phonemic and tonal transcription in a language documentation setting where limited resources are available for model training. We use the connectionist temporal classification (CTC) formulation (Graves et al., 2006) for the purposes of direct prediction of phonemes and tones given an acoustic signal, thus bypassing the need for a pronunciation lexicon, language model, and time alignments of phonemes in the training data. By reducing the data requirements we make automatic transcription technology more feasible in a language documentation setting.";
 
 // $reply = "uncomment the next line of code to call chat gpt";
 $reply = ask_chat_gpt("[".$task1."] ".$text);
@@ -12,7 +32,7 @@ $reply = ask_chat_gpt("[".$task1."] ".$text);
 $task2 = "Select 5 words (terms) from the following text. Choose the words which may be difficult for children to understand. Explain these words in simple terms. Write each word and its explanation on one line, separating the word and explanation by : symbol, use | symbol at the end of each line.";
 
 // $terms = "uncomment the next line of code to call chat gpt";
-$terms = ask_chat_gpt("[".$task2."] ".str_replace("\n", " ", $reply));
+$terms = ask_chat_gpt("[".$task2."] ".str_replace("\n", " ", $text));
 
 $start = "P%";
 
@@ -64,12 +84,10 @@ foreach ($terms_array as $term) {
 include("end.php");
 ?>
 
-
-
 <html>
-  <head>
-    <title>Test 3</title>
-  </head>
+    <script>window.location.href = "output.php?response_id=<?=$response_id?>"</script>
+
+  <!--
   <body>
     <h3>REQUEST</h3>
     <p><?= $task1 ?></p>
@@ -89,4 +107,5 @@ include("end.php");
         }
     ?></p>
   </body>
+  -->
 </html>
