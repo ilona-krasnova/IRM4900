acb36380507a585db18258a9b0f42ade46559050
Ilona Krasnova <ilona.a.krasnova@gmail.com>
2024-02-18 15:22:17 -0500

    ..

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 <?php 
 include("begin.php");
-$responses = database("SELECT * FROM response r JOIN text t on (t.id = r.text_id) ORDER BY t.id DESC, r.difficulty_id DESC LIMIT 100");
+$responses = database("SELECT r.id as rid, summary, difficulty_id, original_text, text_id FROM response r JOIN text t on (t.id = r.text_id) ORDER BY t.id DESC, r.difficulty_id DESC LIMIT 100");
 
 console_var("RESPONSES", $responses);
 include("end.php");
@@ -19,7 +19,7 @@ include("end.php");
                     if ($resp['text_id'] != $last_text_id) {
                         echo '<div style="margin-top:10px;">'.strtok($resp['original_text'], '.').'</div>';
                     }
-                    echo ''.$resp['difficulty_id'].') <a href="output.php?response_id='.$resp['id'].'">'.strtok($resp['summary'],'.').'</a>';
+                    echo ''.$resp['difficulty_id'].') <a href="output.php?response_id='.$resp['rid'].'">'.strtok($resp['summary'],'.').'</a>';
                     echo nl2br("\n");
                     $last_text_id = $resp['text_id'];
                 }
