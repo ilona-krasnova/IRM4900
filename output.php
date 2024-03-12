441abbd7216831c601e3b86f086f3ef2fa6d1e00
Milajj <149986465+Milajj@users.noreply.github.com>
2024-03-12 11:19:12 -0400

    Update output.php

--- output.php
+++ output.php
@@ -47,7 +47,7 @@ include("end.php");
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Rescribe: Summary</title>
-        <link rel="stylesheet" href="rescribe-css.css">
+        <link rel="stylesheet" href="styles.css">
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Dosis:wght@500&family=Quicksand:wght@600&display=swap');
         </style>
