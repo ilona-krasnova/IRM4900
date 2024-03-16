d781aeb83187d291b497a88cca6fab9ae862d37c
Milajj <149986465+Milajj@users.noreply.github.com>
2024-03-16 12:47:37 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,6 @@
-<div class="header">
-                <img src="images/icons8-feather-48.png" class="featherIcon" alt="Rescribe icon - a feather">        
-                <a href="index.php" id="rescribe">RESCRIBE</a>
-                    <div class="headerRight">
-                        <a href="index.php">HOME</a>
-                        <a href="rescribe-about.php">ABOUT</a>
-                        <a href="rescribe-contact.php">CONTACT</a>
-                    </div><!--headerRight end-->
-            </div><!--header end-->
+          <div class="header">
+                <a href="index.php" id="rescribe"><img src="images/icons8-feather-48.png" class="featherIcon" alt="Rescribe icon - a feather">RESCRIBE</a>
+                <a href="index.php">HOME</a>
+                <a href="about.php">ABOUT</a>
+                <a href="contact.php">CONTACT</a>
+            </div><!--end of header-->
