d2f7e691edb010bd2079333437c810b2667779e0
Milajj <149986465+Milajj@users.noreply.github.com>
2024-03-17 16:24:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -16,14 +16,8 @@
 
     <!--Head element used to contain metadata, estabilish the website's title and import google fonts-->
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Rescribe</title>
-        <link rel="stylesheet" href="styles.css?x=2">
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Dosis:wght@500&family=Quicksand:wght@600&display=swap');
-        </style>
-        <link rel="icon" href="images/icons8-feather-48.png" />
+        <?php include("head.php"); ?>
+        <title>Rescribe: Homepage</title>
     </head>
 
     <body>
