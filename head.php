e01aa1fa20ad92870b53db5c8766d0b67687ee03
Milajj <149986465+Milajj@users.noreply.github.com>
2024-03-17 16:15:41 -0400

    Add files via upload

---
+++ head.php
@@ -0,0 +1,9 @@
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Dosis:wght@500&family=Quicksand:wght@600&display=swap');
+        </style>
+        <link rel="icon" href="images/icons8-feather-48.png" />
+    </head>
