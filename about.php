f85e38b80cf08985ab9fd4ae3e724cef35638fa3
Milajj <149986465+Milajj@users.noreply.github.com>
2024-03-17 16:23:18 -0400

    Update about.php

--- about.php
+++ about.php
@@ -14,15 +14,10 @@
 
     <html lang="en">
 
+        <!--Head element used to contain metadata, estabilish the website's title and import google fonts-->
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Rescribe</title>
-            <link rel="stylesheet" href="styles.css">
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Quicksand:wght@300..700&display=swap');
-            </style>
-            <link rel="icon" href="images/icons8-feather-48.png" />
+            <?php include("head.php"); ?>
+            <title>Rescribe: About</title>
         </head>
 
         <body>
