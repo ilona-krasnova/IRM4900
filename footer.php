9a7543883f7280f7507c64c4c2809c731970c389
Milajj <149986465+Milajj@users.noreply.github.com>
2024-03-19 21:13:48 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
         <a href="terms.php">Terms of use</a> &nbsp;|&nbsp;
         <a href="privacy.php">Privacy statement</a> &nbsp;|&nbsp;
         <a href="list.php">Response Log</a> &nbsp;|&nbsp;
-        <a href="rescribe-promo.php">Promotion</a>
+        <a href="promo.php">Promotion</a>
 </div> <!--end of footer-->
